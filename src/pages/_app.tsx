3202075590384440e08a6540cee3f4d0bddc3cbd
JuhaszZoltanAntal <48652435+JuhaszZoltanAntal@users.noreply.github.com>
2023-02-24 19:28:43 +0100

    Exclude register page from the childrens of layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,23 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import Layout from '@/components/layout/Layout';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const router = useRouter();
+
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <>
+        {router.pathname !== '/auth/register' ? (
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        ) : (
+          <Component {...pageProps} />
+        )}
+      </>
     </SessionProvider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,4 @@
-import LoginForm from '@/components/auth/LoginForm';
-import { useSession, signOut } from 'next-auth/react';
 
 export default function Home() {
-  const { data: session } = useSession();
-
-  if (session) {
-    return (
-      <div>
-        <p>Welcome, {session.user?.name}</p>
-        <p>User_id: {session.user?.id}</p>
-        <button
-          onClick={() => {
-            signOut();
-          }}
-        >
-          Sign out
-        </button>
-      </div>
-    );
-  } else {
-    return <LoginForm />;
-  }
+  return <div>Home</div>;
 }
