80b235e6a315d7d97f650036f9f10035a41d0300
JuhaszZoltanAntal <48652435+JuhaszZoltanAntal@users.noreply.github.com>
2023-03-02 17:34:48 +0100

    Add data and user initialization

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,84 @@
-
-import { useEffect } from "react";
+import { useEffect, useContext } from 'react';
 import axios from 'axios';
-import { useSession } from "next-auth/react"
+import { useSession } from 'next-auth/react';
+import STARTING_DATA from '@/mongo/starting-data';
+import UserContext from '@/store/user-context';
+import router from 'next/router';
 
 export default function Home() {
-  const { data: session } = useSession()
-
+  const { data: session } = useSession();
+  const { init, diets } = useContext(UserContext);
 
   useEffect(() => {
-    if(session){
-      axios
-      .post(
-        '/api/user/user',
-        {
-          name: session.user.name,
-          email: session.user.email,
-          id: session.user.id,
-        },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      )
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    }
-  }, [])
-  
+    (async () => {
+      if (session) {
+        //create user in the database if signed in with google oauth
+        await axios
+          .post(
+            '/api/user',
+            {
+              name: session.user.name,
+              email: session.user.email,
+              id: session.user.id,
+            },
+            {
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            }
+          )
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
 
+        //add starting meals and ingredients (this could be set in the database but I want to allow other to set the app locally)
+        await axios
+          .post(
+            '/api/init',
+            {
+              userId: session.user.id,
+              ...STARTING_DATA,
+            },
+            {
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            }
+          )
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
 
-  return <div>Home</div>;
+        //initialize app level data
+        await axios
+          .get('/api/init/' + session.user.id)
+          .then(function (response) {
+            console.log(response);
+            init(response.data.diets, response.data.meals, response.data.ingredients);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }
+    })();
+  }, []);
+
+  return diets && diets.length > 0 ? (
+    <ul>
+      {diets.map((diet) => (
+        <li key={diet.name}>{diet.name}</li>
+      ))}
+    </ul>
+  ) : (
+    <div>
+      <p>You dont have any diet yet, please generate one.</p>
+      <button onClick={() => router.push('/generate-diet')}>Generate a new diet</button>
+    </div>
+  );
 }
