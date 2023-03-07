b7eb122c576543dd44ac27dbafe210572022c18c
JuhaszZoltanAntal <48652435+JuhaszZoltanAntal@users.noreply.github.com>
2023-03-07 19:46:20 +0100

    Add methods to user context and init app data

--- src/store/user-context.tsx
+++ src/store/user-context.tsx
@@ -1,66 +1,197 @@
 import { IDiet } from '@/mongo/models/dietModel';
 import { IIngredient } from '@/mongo/models/ingredientModel';
 import { IMeal } from '@/mongo/models/mealModel';
-import { createContext, useState } from 'react';
+import STARTING_DATA from '@/mongo/starting-data';
+import axios from 'axios';
+import { useSession } from 'next-auth/react';
+import { createContext, useState, useEffect } from 'react';
 
 type UserContexType = {
   diets: IDiet[];
   meals: IMeal[];
   ingredients: IIngredient[];
-  init: (diets: IDiet[], meals: IMeal[], ingredients: IIngredient[]) => void;
   addMeal: (newMeal: IMeal) => void;
+  addIngredient: (newIngredient: IIngredient) => void;
+  generateDiet: (data: {
+    name: string;
+    expectedCaloriesPerDay: number;
+    mealsPerDay: number;
+  }) => void;
 };
 
 const defaultState = {
   diets: [],
   meals: [],
   ingredients: [],
-  init: (diets: IDiet[], meals: IMeal[], ingredients: IIngredient[]) => {},
   addMeal: (newMeal: IMeal) => {},
+  addIngredient: (newIngredient: IIngredient) => {},
+  generateDiet: (data: { name: string; expectedCaloriesPerDay: number; mealsPerDay: number }) => {},
 };
 
 const UserContext = createContext<UserContexType>(defaultState);
 
 export function UserContextProvider({ children }: { children: React.ReactNode }) {
+  const { data: session } = useSession();
+
   const [diets, setDiets] = useState<IDiet[]>([]);
   const [meals, setMeals] = useState<IMeal[]>([]);
   const [ingredients, setIngredients] = useState<IIngredient[]>([]);
 
-  function initHandler(diets: IDiet[], meals: IMeal[], ingredients: IIngredient[]) {
-   
-   
-    console.log('initHandler runinng');
-
-    setDiets(() => {
-      return diets;
-    });
-
-    setMeals(() => {
-      return meals;
-    });
-
-    setIngredients(() => {
-      return ingredients;
-    });
-  }
-
   function addMealHandler(newMeal: IMeal) {
     setMeals((preveousMeals) => {
       if (preveousMeals) {
-        return { ...preveousMeals, newMeal };
+        return [...preveousMeals, newMeal];
       } else {
         return [];
       }
     });
+
+    axios
+      .post(
+        '/api/meal/' + session?.user.id,
+        {
+          ...newMeal,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      )
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  function addIngredientHandler(newIngredient: IIngredient) {
+    setIngredients((preveousIngredients) => {
+      if (preveousIngredients) {
+        return [...preveousIngredients, newIngredient];
+      } else {
+        return [];
+      }
+    });
+
+    axios
+      .post(
+        '/api/ingredient/' + session?.user.id,
+        {
+          ...newIngredient,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      )
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  function generateDietHandler(data: {
+    name: string;
+    expectedCaloriesPerDay: number;
+    mealsPerDay: number;
+  }) {
+    axios
+      .post(
+        '/api/diet/' + session?.user.id,
+        {
+          ...data,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      )
+      .then(function (response) {
+        console.log(response);
+        //todo push the generated diet here
+        setDiets([]);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   const context = {
     diets: diets,
     meals: meals,
     ingredients: ingredients,
-    init: initHandler,
     addMeal: addMealHandler,
+    addIngredient: addIngredientHandler,
+    generateDiet: generateDietHandler,
   };
+
+  useEffect(() => {
+    if (session) {
+      (async () => {
+        //create user in the database if signed in with google oauth
+        await axios
+          .post(
+            '/api/user',
+            {
+              name: session.user.name,
+              email: session.user.email,
+              id: session.user.id,
+            },
+            {
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            }
+          )
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+
+        //add starting meals and ingredients (this could be set in the database but I want to allow others to set the app locally)
+        await axios
+          .post(
+            '/api/init',
+            {
+              userId: session.user.id,
+              ...STARTING_DATA,
+            },
+            {
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            }
+          )
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+
+        //initialize app level data
+        await axios
+          .get('/api/init/' + session.user.id)
+          .then(function (response) {
+            console.log(response);
+            setMeals(response.data.meals);
+            setDiets(response.data.diets);
+            setIngredients(response.data.ingredients);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      })();
+    }
+  }, [session]);
 
   return <UserContext.Provider value={context}>{children}</UserContext.Provider>;
 }
