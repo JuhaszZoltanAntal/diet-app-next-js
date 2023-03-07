a1b14ea932dee3f8b3a3d8625b0399ec8cf8fe06
JuhaszZoltanAntal <48652435+JuhaszZoltanAntal@users.noreply.github.com>
2023-03-07 19:45:28 +0100

    Relocate init routes from home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,73 +1,9 @@
-import { useEffect, useContext } from 'react';
-import axios from 'axios';
-import { useSession } from 'next-auth/react';
-import STARTING_DATA from '@/mongo/starting-data';
+import { useContext } from 'react';
 import UserContext from '@/store/user-context';
 import router from 'next/router';
 
 export default function Home() {
-  const { data: session } = useSession();
-  const { init, diets } = useContext(UserContext);
-
-  useEffect(() => {
-    (async () => {
-      if (session) {
-        //create user in the database if signed in with google oauth
-        await axios
-          .post(
-            '/api/user',
-            {
-              name: session.user.name,
-              email: session.user.email,
-              id: session.user.id,
-            },
-            {
-              headers: {
-                'Content-Type': 'application/json',
-              },
-            }
-          )
-          .then(function (response) {
-            console.log(response);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-
-        //add starting meals and ingredients (this could be set in the database but I want to allow other to set the app locally)
-        await axios
-          .post(
-            '/api/init',
-            {
-              userId: session.user.id,
-              ...STARTING_DATA,
-            },
-            {
-              headers: {
-                'Content-Type': 'application/json',
-              },
-            }
-          )
-          .then(function (response) {
-            console.log(response);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-
-        //initialize app level data
-        await axios
-          .get('/api/init/' + session.user.id)
-          .then(function (response) {
-            console.log(response);
-            init(response.data.diets, response.data.meals, response.data.ingredients);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-      }
-    })();
-  }, []);
+  const { diets } = useContext(UserContext);
 
   return diets && diets.length > 0 ? (
     <ul>
