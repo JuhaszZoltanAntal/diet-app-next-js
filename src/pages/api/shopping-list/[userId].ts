87d9282295452f8be8882d339dc9acfa36462030
JuhaszZoltanAntal <48652435+JuhaszZoltanAntal@users.noreply.github.com>
2023-03-12 12:04:30 +0100

    Create shopping list api route

---
+++ src/pages/api/shopping-list/[userId].ts
@@ -0,0 +1,86 @@
+import type { NextApiHandler } from 'next';
+import { conncetMongo } from '@/mongo/connect';
+import User from '@/mongo/models/userModel';
+import { Unit } from '@/mongo/models/enums';
+import { IIngredient } from '@/mongo/models/ingredientModel';
+import { IDiet } from '@/mongo/models/dietModel';
+import { IMeal } from '@/mongo/models/mealModel';
+import { IDailyMeals } from '@/mongo/models/dailyMealsModel';
+
+let handler: NextApiHandler<any> = async (req, res) => {
+  await conncetMongo();
+
+  if (req.method === 'POST') {
+
+    const checkboxValues = req.body.checkboxValues;
+
+    const user = await User.findOne({ id: req.query.userId });
+
+    const selectedDiets = user.diets.filter((diet: IDiet, index: number) => checkboxValues[index] === true);
+    const selectedDailyMelasList = selectedDiets.flat().map(({ dailyMelasList }: {dailyMelasList: IDailyMeals[]}) => dailyMelasList);
+    const selectedMealsList = selectedDailyMelasList.flat().map(({ mealsList }: {mealsList: IMeal[]}) => mealsList);
+    const selectedIngredients = selectedMealsList.flat().map(({ ingredients }: {ingredients: IIngredient[]}) => ingredients);
+    const selectedIngredientsFlattened = selectedIngredients.flat();
+
+    const standardizedIngredients: IIngredient[] = [];
+    selectedIngredientsFlattened.forEach((ingredient: IIngredient) => {
+      let multiplier;
+      let newUnit;
+      switch (ingredient.unit) {
+        case Unit.dkg:
+          multiplier = 10;
+          newUnit = Unit.g;
+          break;
+        case Unit.dl:
+          multiplier = 100;
+          newUnit = Unit.ml;
+          break;
+        case Unit.kg:
+          multiplier = 1000;
+          newUnit = Unit.g;
+          break;
+        case Unit.l:
+          multiplier = 1000;
+          newUnit = Unit.ml;
+          break;
+        default:
+          multiplier = 1;
+          newUnit = ingredient.unit;
+          break;
+      }
+
+      ingredient.amount = ingredient.amount! * multiplier;
+      ingredient.unit = newUnit;
+
+      standardizedIngredients.push(ingredient);
+    });
+
+    let groupedAndSummedIngredients: IIngredient[] = [];
+    standardizedIngredients.reduce((res, value) => {
+      if (!(res as any)[value.name]) {
+        (res as any)[value.name] = { name: value.name, amount: 0 };
+        groupedAndSummedIngredients.push((res as any)[value.name]);
+      }
+      (res as any)[value.name].amount += value.amount;
+      (res as any)[value.name].unit = value.unit;
+      return res;
+    }, {});
+
+    if (user) {
+      const filter = { id: req.query.userId };
+      const update = { $push: { shoppingList: groupedAndSummedIngredients } };
+      let doc = await User.findOneAndUpdate(filter, update);
+
+      res.status(200).json({
+        message: 'Shopping List successfully added.',
+        newShoppingList: groupedAndSummedIngredients,
+      });
+    } else {
+      res.status(500).send({
+        message: 'Something went wrong.',
+      });
+    }
+  }
+};
+
+export default handler;
